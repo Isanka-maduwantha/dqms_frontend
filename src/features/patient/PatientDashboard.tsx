98270641ff601bef004d28f3a2766d9caba96ea5
isanka <maduwanthaisanka@gmail.com>
2026-08-07 13:13:29 +0530

    Commit latest

--- src/features/patient/PatientDashboard.tsx
+++ src/features/patient/PatientDashboard.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import CommanButton from "../../components/CommanButton";
 
 function PatientDashboard() {
   return (
-    <div>PatientDashboard</div>
-  )
+    <div>
+      <h1 className="text-left">Appointment Management</h1>
+      <p>Cancellations within 24 hours may incur a clinical service fee.</p>
+      <CommanButton  label="Book new appointment"/>
+    </div>
+  );
 }
 
-export default PatientDashboard
+export default PatientDashboard;
